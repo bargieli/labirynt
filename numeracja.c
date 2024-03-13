f58c617e506f651f41246b84e481e976a9f6076f
bargieli <153275441+bargieli@users.noreply.github.com>
2024-03-13 23:22:34 +0100

    Tworzenie pliku z odległościami 1.0
    Funkcje zawarte w pliku "numeracja.c" pozwalają na uzyskanie pliku binarnego "plik_programu.txt" zawierającego odległości od wejścia liczone po labiryncie. UWAGI: "plik_programu.txt" musi istnieć na początku i poza zakresem pliku dotyczącym aktualnego wywołania może zawierać dane z poprzednich uruchomień

---
+++ numeracja.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//IZABELA BARGIEŁ
+
+
+int zwroc_wartosc_x_y(const char* nazwa_pliku, int x, int y, int liczba_kolumn) {
+    FILE* plik_roboczy = fopen(nazwa_pliku, "rb");
+    
+    if (plik_roboczy == NULL) {
+        fprintf(stderr, "Nie udało się wczytać pliku! \n");
+        return -10;
+    }
+     
+    // pozycja kursora
+    long pozycja = (long)(y * liczba_kolumn + x) * sizeof(int);
+    fseek(plik_roboczy, pozycja, SEEK_SET);
+
+    int wartosc;
+    fread(&wartosc, sizeof(int), 1, plik_roboczy);
+
+    fclose(plik_roboczy);
+    return wartosc;
+}
+
+
+void wydrukuj_plik_binarny(const char* nazwa_pliku, int ile_lin, int dl_lin){
+    printf("\n\n");
+    for(int i=0; i<ile_lin; i++){
+        for(int j=0; j<dl_lin; j++){
+            printf("%d ", zwroc_wartosc_x_y(nazwa_pliku, j, i, dl_lin));
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+    
+}
+
+
+
+
+void zmien_wartosc_x_y(const char* nazwa_pliku, int x, int y, int nw_wart, int liczba_kolumn) {
+    FILE* plik_roboczy = fopen(nazwa_pliku, "rb+");
+    
+    if (plik_roboczy == NULL) {
+        fprintf(stderr, "Nie udało się wczytać pliku! \n");
+        return;
+    }
+    
+    int wartosc;
+    long pozycja;
+
+    pozycja = (long)(y * liczba_kolumn + x) * sizeof(int);
+
+    fseek(plik_roboczy, pozycja, SEEK_SET);
+    fread(&wartosc, sizeof(int), 1, plik_roboczy);
+    
+    wartosc = nw_wart;
+
+    fseek(plik_roboczy, pozycja, SEEK_SET);
+    fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+
+    fclose(plik_roboczy);
+}
+
+
+
+void odwiedz(const char* nazwa_pliku, int x, int y, int wart_akt, int liczba_kolumn, int liczba_wierszy){
+    
+    zmien_wartosc_x_y(nazwa_pliku, x, y, wart_akt, liczba_kolumn);
+    
+    
+    if(x<liczba_kolumn-1){
+        if(zwroc_wartosc_x_y(nazwa_pliku, x+1, y, liczba_kolumn)==(-2) ||  zwroc_wartosc_x_y(nazwa_pliku, x+1, y, liczba_kolumn)>wart_akt+1){
+            odwiedz(nazwa_pliku, x+1, y, wart_akt+1, liczba_kolumn, liczba_wierszy);
+        }
+    }
+    
+    if(y<liczba_wierszy-1){
+        if(zwroc_wartosc_x_y(nazwa_pliku, x, y+1, liczba_kolumn)==(-2) ||  zwroc_wartosc_x_y(nazwa_pliku, x, y+1, liczba_kolumn)>wart_akt+1){
+            odwiedz(nazwa_pliku, x, y+1, wart_akt+1, liczba_kolumn, liczba_wierszy);
+        }
+    }
+    
+    if(x>0){
+        if(zwroc_wartosc_x_y(nazwa_pliku, x-1, y, liczba_kolumn)==(-2) ||  zwroc_wartosc_x_y(nazwa_pliku, x-1, y, liczba_kolumn)>wart_akt+1){
+            odwiedz(nazwa_pliku, x-1, y, wart_akt+1, liczba_kolumn, liczba_wierszy);
+        }
+    }
+    
+    if(y>0){
+        if(zwroc_wartosc_x_y(nazwa_pliku, x, y-1, liczba_kolumn)==(-2) ||  zwroc_wartosc_x_y(nazwa_pliku, x, y-1, liczba_kolumn)>wart_akt+1){
+            odwiedz(nazwa_pliku, x, y-1, wart_akt+1, liczba_kolumn, liczba_wierszy);
+        }
+    }
+    
+    return;
+}
+ 
+ 
+
+int main(int argc, char** argv){
+    FILE* plik = fopen(argv[1], "r");
+    
+    if(plik == NULL){
+        fprintf(stderr, "Nie udało się wczytać pliku! \n");
+        return 1;
+    }
+    
+    FILE* plik_roboczy = fopen("plik_programu.txt", "rb+");
+    
+    int x0, y0;
+    int licz =0;
+    int dl_lin=-1;
+    
+    char a;
+    while((a=fgetc(plik)) != EOF){
+        
+        // sciana
+        if(a=='X'){
+            int wartosc = -1;
+            fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+            licz++;
+        }
+        
+        //poczatek
+        else if(a=='P'){
+            x0 = licz;
+            y0 = licz;
+            int wartosc = 0;
+            fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+            licz++;
+        }
+        
+        //puste pole
+        else if(a==' '){
+            int wartosc = -2;
+            fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+            licz++;
+        }
+        
+        //koniec
+        else if(a=='K'){
+            int wartosc = -3;
+            fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+            licz++;
+        }
+        
+        else {
+            if(dl_lin==-1){
+                dl_lin = licz;
+            }
+        }
+    }
+    fclose(plik_roboczy);
+    fclose(plik);
+    
+    
+    
+    x0%=dl_lin;
+    y0=y0/dl_lin;
+    int ile_lin = licz/dl_lin;
+
+    
+    plik_roboczy = fopen("plik_programu.txt", "rb+");
+    odwiedz("plik_programu.txt", x0, y0, 0, dl_lin, ile_lin);
+    
+    wydrukuj_plik_binarny("plik_programu.txt", ile_lin, dl_lin);
+    
+    fclose(plik_roboczy);
+    fclose(plik);
+    
+    return 0;
+}
