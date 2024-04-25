c44483f50cf5b2e94fdc8898caeb903ad569389f
azielinska <azielinska@ubuntu.myguest.virtualbox.org>
2024-04-25 11:16:55 +0200

    dodanie opisow funkcji

--- numeracja.c
+++ numeracja.c
@@ -5,7 +5,7 @@
 //IZABELA BARGIEŁ
 
 
-int zwroc_wartosc_x_y(const char* nazwa_pliku, int x, int y, int liczba_kolumn) {
+int zwroc_wartosc_x_y(const char* nazwa_pliku, int x, int y, int liczba_kolumn) { //zwraca wartość liczbową pola o konkretnych współrzędnych (x, y) z labiryntu zapisanego w formie pliku binarnego
 
     FILE* plik_roboczy = fopen(nazwa_pliku, "rb");
     
@@ -27,7 +27,8 @@ int zwroc_wartosc_x_y(const char* nazwa_pliku, int x, int y, int liczba_kolumn) 
 }
 
 
-void wydrukuj_plik_binarny(const char* nazwa_pliku, int ile_lin, int dl_lin){
+void wydrukuj_plik_binarny(const char* nazwa_pliku, int ile_lin, int dl_lin){ //funkcja testowa, drukuje zdekodowane wartości znajdujące się w pliku binarnym
+
     printf("\n\n");
     for(int i=0; i<ile_lin; i++){
         for(int j=0; j<dl_lin; j++){
@@ -42,7 +43,8 @@ void wydrukuj_plik_binarny(const char* nazwa_pliku, int ile_lin, int dl_lin){
 
 
 
-void zmien_wartosc_x_y(const char* nazwa_pliku, int x, int y, int nw_wart, int liczba_kolumn) {
+void zmien_wartosc_x_y(const char* nazwa_pliku, int x, int y, int nw_wart, int liczba_kolumn) { ///modyfikuje pole o podanych współrzędnych (x, y) w pliku binarnym nadając mu nową wartość (nw_wart)
+
     FILE* plik_roboczy = fopen(nazwa_pliku, "rb+");
     
     if (plik_roboczy == NULL) {
@@ -68,7 +70,8 @@ void zmien_wartosc_x_y(const char* nazwa_pliku, int x, int y, int nw_wart, int l
 
 
 
-void odwiedz(const char* nazwa_pliku, int x, int y, int wart_akt, int liczba_kolumn, int liczba_wierszy){
+void odwiedz(const char* nazwa_pliku, int x, int y, int wart_akt, int liczba_kolumn, int liczba_wierszy){ //nadaje polu o współrzędnych (x, y) wartość jego odległości od punktu początkowego (wart_akt) i odsyłająca do jego sąsiadów, aby im również nadać wartość (wart_akt+1)
+
     
     zmien_wartosc_x_y(nazwa_pliku, x, y, wart_akt, liczba_kolumn);
     
@@ -100,7 +103,8 @@ void odwiedz(const char* nazwa_pliku, int x, int y, int wart_akt, int liczba_kol
     return;
 }
 
-int wczytaj(char *nazwa_pliku, int *lini)
+int wczytaj(char *nazwa_pliku, int *lini) //poddaje plik wejściowy pierwszej obróbce - zamienia znaki na liczby i zapisuje w pliku binarnym o podanej nazwie (nazwa_pliku)
+
  {
      FILE* plik = fopen(nazwa_pliku, "r");
     
