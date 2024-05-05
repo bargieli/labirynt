0ddeeeac0c9ba1a5a38f406e270ac2339271c80f
bargieli <153275441+bargieli@users.noreply.github.com>
2024-05-05 21:52:25 +0200

    BFS iteracyjnie
    dziala fantastycznie

--- numeracja.c
+++ numeracja.c
@@ -5,7 +5,7 @@
 //IZABELA BARGIEŁ
 
 
-#define MAX_SIZE (511900/4)
+#define MAX_SIZE ((512000/4)-10200)
 
 typedef struct {
     int data[MAX_SIZE];
@@ -24,12 +24,13 @@ Queue* createQueue() {
     return q;
 }
 
+// sprawdza czy kolejka jest pusta
 int isEmpty(Queue* q) {
     return (q->front == -1 && q->rear == -1);
 }
 
+// dodaje element do kolejki
 void q_dodaj(Queue* q, int value) {
-    
     if ((q->rear + 1) % MAX_SIZE == q->front) {
         fprintf(stderr, "Kolejka pełna!\n");
         exit(EXIT_FAILURE);
@@ -41,6 +42,7 @@ void q_dodaj(Queue* q, int value) {
     q->data[q->rear] = value;
 }
 
+// zwraca i usuwa pierwszy element kolejki
 int q_pop(Queue* q) {
     if (isEmpty(q)) {
         fprintf(stderr, "Kolejka pusta!\n");
@@ -56,12 +58,13 @@ int q_pop(Queue* q) {
     }
 }
 
+// zwalnia pamiec uzywana przez kolejke
 void freeQueue(Queue* q) {
     free(q);
 }
 
-int zwroc_wartosc_x_y(const char* nazwa_pliku, int x, int y, int liczba_kolumn) { //zwraca wartość liczbową pola o konkretnych współrzędnych (x, y) z labiryntu zapisanego w formie pliku binarnego
-
+// zwraca wartość liczbową pola o konkretnych współrzędnych (x, y) z pliku binarnego
+int zwroc_wartosc_x_y(const char* nazwa_pliku, int x, int y, int liczba_kolumn) { 
     FILE* plik_roboczy = fopen(nazwa_pliku, "rb");
     
     if (plik_roboczy == NULL) {
@@ -81,45 +84,8 @@ int zwroc_wartosc_x_y(const char* nazwa_pliku, int x, int y, int liczba_kolumn) 
     return wartosc;
 }
 
-/*
-int q_pop(const char* nazwa_pliku) { //zwraca wartość liczbową pola o konkretnych współrzędnych (x, y) z labiryntu zapisanego w formie pliku binarnego
-    FILE* plik = fopen(nazwa_pliku, "rb+");
-
-    if (plik == NULL) {
-        fprintf(stderr, "Nie udało się otworzyć pliku %s! STOS_POP\n", nazwa_pliku);
-        exit(-1);
-    }
-
-    int ostatnia = 0;
-    fseek(plik, -sizeof(int), SEEK_END);
-    fread(&ostatnia, sizeof(int), 1, plik);
-    ftruncate(fileno(plik), ftell(plik) - sizeof(int));
-    fclose(plik);
-
-    return ostatnia;
-}
-
-
-void dodaj_wartosc_do_pliku(const char* nazwa_pliku, int wartosc) {
-    FILE* plik = fopen(nazwa_pliku, "ab"); // Otwórz plik do dopisywania na koniec w trybie binarnym
-    
-    if (plik == NULL) {
-        fprintf(stderr, "Nie udało się otworzyć pliku! DODAJ WART\n");
-        exit(-1);
-    }
-    
-    // Ustaw kursor na koniec pliku
-    fseek(plik, 0, SEEK_END);
-    
-    // Zapisz wartość na koniec pliku
-    fwrite(&wartosc, sizeof(int), 1, plik);
-    
-    fclose(plik);
-}
-*/
-
-
-void zmien_wartosc_x_y(const char* nazwa_pliku, int x, int y, int nw_wart, int liczba_kolumn) { //modyfikuje pole o podanych współrzędnych (x, y) w pliku binarnym nadając mu nową wartość (nw_wart)
+// modyfikuje pole o podanych współrzędnych (x, y) w pliku binarnym nadając mu nową wartość (nw_wart)
+void zmien_wartosc_x_y(const char* nazwa_pliku, int x, int y, int nw_wart, int liczba_kolumn) { 
 
     FILE* plik_roboczy = fopen(nazwa_pliku, "rb+");
     
@@ -144,34 +110,31 @@ void zmien_wartosc_x_y(const char* nazwa_pliku, int x, int y, int nw_wart, int l
     fclose(plik_roboczy);
 }
 
-
+// zapisuje labirynt w postaci pliku binarnego z odleglosciami od startu
 void odwiedz(const char* nazwa_pliku, int x, int y, int liczba_kolumn, int liczba_wierszy) {
     Queue* q = createQueue();
 
     zmien_wartosc_x_y(nazwa_pliku, x, y, 0, liczba_kolumn);
-    q_dodaj(q, x + y * liczba_kolumn); // Dodanie współrzędnych startowych do kolejki
+    q_dodaj(q, x + y * liczba_kolumn);
 
     while (!isEmpty(q)) {
         int akt_pole = q_pop(q);
         x = akt_pole % liczba_kolumn;
         y = akt_pole / liczba_kolumn;
 
-        // Tablice przesunięć
         int dx[] = {1, -1, 0, 0};
         int dy[] = {0, 0, 1, -1};
 
-        // Iteracja po wszystkich sąsiadach
         for (int i = 0; i < 4; ++i) {
             int new_x = x + dx[i];
             int new_y = y + dy[i];
 
-            // Sprawdzenie czy sąsiad jest w granicach labiryntu
             if (new_x >= 0 && new_x < liczba_kolumn && new_y >= 0 && new_y < liczba_wierszy) {
-                // Sprawdzenie czy sąsiad nie jest ścianą oraz czy nie został już odwiedzony
                 int wartosc = zwroc_wartosc_x_y(nazwa_pliku, new_x, new_y, liczba_kolumn);
-                if (wartosc != -1 && wartosc == -2) {
-                    q_dodaj(q, new_x + new_y * liczba_kolumn); // Dodanie sąsiada do kolejki
-                    zmien_wartosc_x_y(nazwa_pliku, new_x, new_y, zwroc_wartosc_x_y(nazwa_pliku, x, y, liczba_kolumn) + 1, liczba_kolumn); // Aktualizacja odległości
+                // sprawdzenie czy sąsiad nie został odwiedzony
+                if (wartosc == -2) {
+                    q_dodaj(q, new_x + new_y * liczba_kolumn); 
+                    zmien_wartosc_x_y(nazwa_pliku, new_x, new_y, zwroc_wartosc_x_y(nazwa_pliku, x, y, liczba_kolumn) + 1, liczba_kolumn); 
                 }
             }
         }
@@ -182,60 +145,73 @@ void odwiedz(const char* nazwa_pliku, int x, int y, int liczba_kolumn, int liczb
 
  
  
- 
+// zamienia znaki na liczby i zapisuje w pliku binarnym o podanej nazwie (nazwa_pliku) 
 int wczytaj(char *nazwa_pliku, int *lini) {
-    FILE* plik = fopen(nazwa_pliku, "r");
+     FILE* plik = fopen(nazwa_pliku, "r");
     
-    if (plik == NULL) {
-        fprintf(stderr, "Nie udało się wczytać pliku! WCZYTAJ\n");
+    if(plik == NULL){
+        fprintf(stderr, "Nie udało się wczytać pliku! \n");
         exit(-1);
     }
     
-    FILE* plik_roboczy = fopen("plik_programu.txt", "wb+");
+    FILE* plik_roboczy = fopen("plik_programu.txt", "rb+");
     
     int x0, y0;
-    int licz = 0;
-    int dl_lin = -1;
+    int licz =0;
+    int dl_lin=-1;
+    
     char a;
-    int* bufor = (int*)malloc(MAX_SIZE * sizeof(int));
-
-    if (bufor == NULL) {
-        fprintf(stderr, "Błąd alokacji pamięci dla bufora!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    while ((a = fgetc(plik)) != EOF) {
-        int wartosc;
-        if (a == 'X') wartosc = -1;
-        else if (a == 'P') {
+    while((a=fgetc(plik)) != EOF){
+        
+        // sciana
+        if(a=='X'){
+            int wartosc = -1;
+            fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+            licz++;
+        }
+        
+        //poczatek
+        else if(a=='P'){
             x0 = licz;
             y0 = licz;
-            wartosc = 0;
-        } else if (a == ' ') wartosc = -2;
-        else if (a == 'K') wartosc = -3;
-        else {
-            if (dl_lin == -1) dl_lin = licz;
-            continue;
+            int wartosc = 0;
+            fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+            licz++;
         }
-
-        bufor[licz++] = wartosc;
+        
+        //puste pole
+        else if(a==' '){
+            int wartosc = -2;
+            fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+            licz++;
+        }
+        
+        //koniec
+        else if(a=='K'){
+            int wartosc = -3;
+            fwrite(&wartosc, sizeof(int), 1, plik_roboczy);
+            licz++;
+        }
+        
+        else {
+            if(dl_lin==-1){
+                dl_lin = licz;
+            }
+        }
     }
-
-    fwrite(bufor, sizeof(int), licz, plik_roboczy);
-
+    
     fclose(plik_roboczy);
     fclose(plik);
+    
+    
+    
+    x0%=dl_lin;
+    y0=y0/dl_lin;
+    int ile_lin = licz/dl_lin;
 
-    x0 %= dl_lin;
-    y0 = y0 / dl_lin;
-    int ile_lin = licz / dl_lin;
-    *lini = ile_lin;
-
+    *lini=ile_lin;
     plik_roboczy = fopen("plik_programu.txt", "rb+");
     odwiedz("plik_programu.txt", x0, y0, dl_lin, ile_lin);
     fclose(plik_roboczy);
-
-    free(bufor);
-    return dl_lin;
-}
-
+        return dl_lin;
+ }
